e409f68dba1458026ca5415d9785536158082131
ornella28 <angelnibaruta88@gmail.com>
2025-12-10 12:59:17 +0100

    making a list

--- src/main/java/se/lexicon/ContactApp.java
+++ src/main/java/se/lexicon/ContactApp.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 
 public class ContactApp {
 
+    static String [] names= new String[10];// accessible everywhere
+    static int count = 0;//the system will count the number of input
+
     Scanner scanner = new Scanner(System.in);
 
     void main(){
+
+        while (true){//keeps showing the menu until user chooses to exit
+
+
         System.out.println("=====Contact Management App========");
         System.out.println("1. Add contacts");
         System.out.println("2. Search by name");
@@ -18,6 +25,7 @@ public class ContactApp {
         //addNames();
 
         //code to choose options
+
         int option = scanner.nextInt();
         scanner.nextLine();// to wait for the input first
 
@@ -26,12 +34,20 @@ public class ContactApp {
             case 1:
                 addNames(scanner);
                 break;// to stop java from continuing
+
+            case 2:
+
+
+            case 3:
+                searchName();
+                break;
+
+            case 4:
+
+
         }
 
-
-
-
-
+        }
 
     }
 
@@ -45,18 +61,34 @@ public class ContactApp {
     }*/
 
     static void addNames(Scanner scanner){
-        //Scanner scanner = new Scanner(System.in);
 
-        String [] names= new String[10];
-
+        if (count== names.length){
+            System.out.println("Constact list is full!");
+            return;
+        }
         System.out.println("Please enter the name: ");
-        names[0] = scanner.nextLine();// asks for the name
+        String name= scanner.nextLine();// asks for the name
 
         System.out.println("Please enter the phone number: ");
-        names[1] = scanner.nextLine();//asks for the phone and saves it
+        String phone= scanner.nextLine();//asks for the phone and saves it
 
-        // we want to check duplicates
-        String[]uniqueNames = new String[10];// new array to store unique names
+
+
+        String newContact = name + "|" + phone;
+
+        for (int i=0; i<count; i++){
+            if (names[i].equalsIgnoreCase(newContact)){
+                System.out.println("The contact already exists!");
+                return;//stops adding names
+            }
+        }
+        names[count]= newContact;
+        count++;// to update the count, otherwise , the list will still be empty
+
+
+
+                // we want to check duplicates
+      /* String[]uniqueNames = new String[10];// new array to store unique names
         int uniqueCount = 0;//how many unique names we have added
         for (int i=0; i< names.length; i++){
             boolean alreadyExists = false;
@@ -73,9 +105,26 @@ public class ContactApp {
                 uniqueCount++;
             }
 
+        }*/
+
+        System.out.println("Contact: " + name + "|" + phone + " saved! ");
+
+
+
+    }
+
+    static void searchName (){
+
+        System.out.println("------List of all names------");
+
+        if (count==0){
+            System.out.println("The list is empty, enter new contact");
+            return;
         }
 
-        System.out.println("Contact: " + names[0] + "|" + names[1] + " saved! ");
+        for (int i = 0; i< count; i++){
+            System.out.println(names[i]);
+        }
 
 
 
