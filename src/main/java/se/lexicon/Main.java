ad19f52e87b0a35c7713c3f5f092917db0c9fd10
ornella28 <angelnibaruta88@gmail.com>
2025-12-11 22:26:50 +0100

    corrections

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -2,13 +2,11 @@ package se.lexicon;
 
 public class Main {
 
-    public static void main(String[]args) {
-        ConsoleUI ui = new ConsoleUI();
-        ui.run();// will start the menu
+
 
 
 
 
 
     }
-}
+
